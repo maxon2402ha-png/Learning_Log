2de81176b6c04f29cba285635a582ee2972d092b
maxon <maxon2402ha@gmail.com>
2026-09-08 20:41:29 +0300

    Add style

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -10,7 +10,7 @@ def index(request):
 
 @login_required
 def topics(request):
-    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_add')
     context = {'topics' : topics}
     return render(request, 'learning_logs/topics.html', context)    
 
